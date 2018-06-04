cb4ef90dadee55a98dcbad70b93704b973d8132a
Genevieve Schultz <genschultz@gmail.com>
2018-06-04 16:51:17 -0700

    more js updates

--- js/script.js
+++ js/script.js
@@ -1,17 +1,23 @@
 $(document).ready(function() {
-  console.log("jQuery ready");
+  // click on square and change the color
   $(".col").click(function () {
-    $(".col").css("background-color", "rebeccapurple");
-    console.log("div clicked");
+    $(this).css("background-color", "rebeccapurple");
+  });
+  $.get("http://thecolorapi.com/id?hex=0047AB&rgb=0,71,171&hsl=215,100%,34%&cmyk=100,58,0,33&format=html", function () {
+    console.log("api requested");
+  }).done(function (returnData) {
+    console.log(returnData);
   });
   //default audio/background music?
 });
 
 // function flipSquare() {
-//   // AJAX query for color API
-//   // Display color on div
-//   // play sound effect?
-//   // use flipSquare in "click" event
+  // AJAX query for color API
+
+  // $.get("http://thecolorapi.com/scheme{mode=monochrome-dark&count=8}");
+  // Display color on div
+  // play sound effect?
+  // use flipSquare in "click" event
 // }
 
 // function checkForMatch() {
@@ -27,12 +33,6 @@ $(document).ready(function() {
 //  Solitaire Mode
 //  Player vs. Player Mode
 //  Player vs. AI Mode
-
-// Click on square event
-// $(".row").click(function() {
-//   // $("div").css("background-color", "rebeccapurple");
-//   console.log("div clicked");
-// });
 
 // Reset Button
 // function reset(e) {
