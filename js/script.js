63d30712af8824fe8d94dce881570a9c6ff5e659
Genevieve Schultz <genschultz@gmail.com>
2018-06-04 15:45:05 -0700

    monday afternoon commit

---
+++ js/script.js
@@ -0,0 +1,40 @@
+$(document).ready(function() {
+  console.log("jQuery ready");
+  $(".col").click(function () {
+    $(".col").css("background-color", "rebeccapurple");
+    console.log("div clicked");
+  });
+  //default audio/background music?
+});
+
+// function flipSquare() {
+//   // AJAX query for color API
+//   // Display color on div
+//   // play sound effect?
+//   // use flipSquare in "click" event
+// }
+
+// function checkForMatch() {
+//   // Check for matching colors when two squares are "flipped"
+  
+// }
+
+// change game instructions in .gameInstruct depending on game mode selected
+//  see p text on index.html
+
+// win conditions
+//  two colored divs need to match exactly 
+//  Solitaire Mode
+//  Player vs. Player Mode
+//  Player vs. AI Mode
+
+// Click on square event
+// $(".row").click(function() {
+//   // $("div").css("background-color", "rebeccapurple");
+//   console.log("div clicked");
+// });
+
+// Reset Button
+// function reset(e) {
+  
+// }
