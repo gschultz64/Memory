18edae63712caca22a9931ec6e75d2691b9e17d0
Genevieve Schultz <genschultz@gmail.com>
2018-06-05 16:13:06 -0700

    committed tuesday files

--- js/script.js
+++ js/script.js
@@ -1,27 +1,54 @@
+var colors = [
+  '#3C989E', '#5DB5A4', '#F4CDA5', '#F57A82', '#E37B40', '#5E005E', '#AB2F52', '#41733F'
+];
+var squares = [];
+
 $(document).ready(function() {
-  // click on square and change the color
-  $(".col").click(function () {
-    $(this).css("background-color", "rebeccapurple");
-  });
-  $.get("http://thecolorapi.com/id?hex=0047AB&rgb=0,71,171&hsl=215,100%,34%&cmyk=100,58,0,33&format=html", function () {
-    console.log("api requested");
-  }).done(function (returnData) {
-    console.log(returnData);
-  });
+  // Initialize Materialize components
+  M.AutoInit();
+  flipSquare();
+  // request API 
+  // $.get()
+  // });
   //default audio/background music?
+  // Button Clicks
+  // Solitaire Button Click
+  $('#solitaire').click(function () {
+    // Set game to play alone
+    // Display Solitaire rules
+    $('p').text("Click to turn over any two squares. If the two squares match, you win! If they don't match, click another square. Repeat as many times as you want!");
+  });
+  // PvP Button Click
+  $('#twoPlayers').click(function () {
+    // Set game to play against another player
+    // Display PvP rules
+    $('p').text("Player 1 starts: Click to turn over any two squares. If the two squares match, you win! If they don't match, Player 2 takes a turn to click two squares. Repeat as many times as you want!");
+  });
+  // PvAi Button Click
+  $('#computer').click(function () {
+    // Set game to play against computer/ai
+    // Display PvAi rules
+    $('p').text("Click to turn over any two squares. If the two squares match, you win! If they don't match, the computer takes a turn to click two squares. Repeat as many times as you want!");
+  });
+  // Reset Button
+  $('#autorenew').click(function () {
+    $('.col').css('background-color', '');
+  });
+  
 });
 
-// function flipSquare() {
-  // AJAX query for color API
-
-  // $.get("http://thecolorapi.com/scheme{mode=monochrome-dark&count=8}");
+function flipSquare() {
+  // click on square and change the color
+  $(".col").click(function () {
+    $(this).css("background-color", colors[0]);
+  });
   // Display color on div
   // play sound effect?
   // use flipSquare in "click" event
-// }
+}
 
 // function checkForMatch() {
-//   // Check for matching colors when two squares are "flipped"
+  // Check for matching colors when two squares are "flipped"
   
 // }
 
@@ -34,7 +61,4 @@ $(document).ready(function() {
 //  Player vs. Player Mode
 //  Player vs. AI Mode
 
-// Reset Button
-// function reset(e) {
-  
-// }
+
