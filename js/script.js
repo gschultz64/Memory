bd56200c3e2d47a3fc8d8f5c2bf19c0961243c20
Genevieve Schultz <genschultz@gmail.com>
2018-06-06 22:47:48 -0700

    made progress on randomized array of colors to fill the squares

--- js/script.js
+++ js/script.js
@@ -2,52 +2,48 @@ var colors = [
   '#3C989E', '#5DB5A4', '#F4CDA5', '#F57A82', '#E37B40', '#5E005E', '#AB2F52', '#41733F'
 ];
 var squares = [
-  $('#box1'), $('#box2'), $('#box3'), $('#box4'),
-  $('#box5'), $('#box6'), $('#box7'), $('#box8'),
-  $('#box9'), $('#box10'), $('#box11'), $('#box12'),
-  $('#box13'), $('#box14'), $('#box15'), $('#box16')
+  '#box1', '#box2', '#box3', '#box4',
+  '#box5', '#box6', '#box7', '#box8',
+  '#box9', '#box10', '#box11', '#box12',
+  '#box13', '#box14', '#box15', '#box16'
 ];
-// New empty array that will hold 2 copies of each color code from colors array
-var moreColors = [];
-moreColors.push(colors);
-moreColors.push(colors);
-var randomColors = moreColors.reduce(function(accumulator, currentValue) {
-  return accumulator.concat(currentValue);
-})
-
-console.log(randomColors);
+// New array that holds 2 copies of each color code from colors array
+var moreColors = colors.concat(colors);
+// Shuffle the order of colors in array
+var randomColors = shuffle(moreColors);
 
 
-//Shuffle colors in array randomColors
-Array.prototype.shuffle = function () {
-  var input = this;
 
-  for (var i = input.length - 1; i >= 0; i--) {
+//Function to shuffle items in array based on Fisher–Yates shuffle
+function shuffle(array) {
 
-    var randomIndex = Math.floor(Math.random() * (i + 1));
-    var itemAtIndex = input[randomIndex];
+  for (var i = array.length - 1; i > 0; i--) {
 
-    input[randomIndex] = input[i];
-    input[i] = itemAtIndex;
+    // Pick a remaining element…
+    var randomIndex = Math.floor(Math.random() * i);
+    var itemAtIndex = array[randomIndex];
+
+    // And swap it with the current element.
+    array[randomIndex] = array[i];
+    array[i] = itemAtIndex;
   }
-  return input;
-}  
+
+  return array;
+}
 
 
 // For loop to randomly add one color for each item from squares array (each square on the board)
 // for (let i = 0; i < squares.length; i++) {
 //   randomColors[i] = colors[Math.floor(Math.random() * colors.length)];
 // }
-// console.log(randomColors);
 // // Limit 2 of each color being put into randomColors
-// $.inArray()
 
 
 $(document).ready(function () {
   // Initialize Materialize components
   M.AutoInit(); 
-  
-  // flipSquare();
+
+  flipSquare();
   
   //default audio/background music?
 
@@ -82,25 +78,22 @@ function flipSquare() {
   
   // click on square and change the color
   $('.col').click(function () {
-      $(this).css("background-color", );
+    //Set random colors for each square
+    for (let i = 0; i < squares.length; i++) {
+      $(squares[i]).css('background-color', randomColors[i]);
+    }
+      // $(this).css("background-color", );
   });
 }
+// if/else statement so that only 1 square has color visible at a time
+// in flipSquare: select color from randomColors when click on square happens
 
-
-
-
-  // Display color on div
-  // play sound effect?
-  // use flipSquare in "click" event
 
 
 // function checkForMatch() {
   // Check for matching colors when two squares are "flipped"
 
 // }
-
-// change game instructions in .gameInstruct depending on game mode selected
-//  see p text on index.html
 
 // win conditions
 //  two colored divs need to match exactly 
