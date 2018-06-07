ffe1a940526a6908438b82f09ca86efc3c189b68
Genevieve Schultz <genschultz@gmail.com>
2018-06-07 10:46:41 -0700

    removed spotify api files

--- js/script.js
+++ js/script.js
@@ -1,20 +1,11 @@
+// global variables
 var colors = [
   '#3C989E', '#5DB5A4', '#F4CDA5', '#F57A82', '#E37B40', '#5E005E', '#AB2F52', '#41733F'
 ];
-var squares = [
-  '#box1', '#box2', '#box3', '#box4',
-  '#box5', '#box6', '#box7', '#box8',
-  '#box9', '#box10', '#box11', '#box12',
-  '#box13', '#box14', '#box15', '#box16'
-];
 // New array that holds 2 copies of each color code from colors array
 var moreColors = colors.concat(colors);
-// Shuffle the order of colors in array
-var randomColors = shuffle(moreColors);
-
-
-
 //Function to shuffle items in array based on Fisher–Yates shuffle
+// https://bost.ocks.org/mike/shuffle/ was helpful
 function shuffle(array) {
 
   for (var i = array.length - 1; i > 0; i--) {
@@ -30,59 +21,23 @@ function shuffle(array) {
 
   return array;
 }
+// Shuffle the order of colors in array
+var randomColors = shuffle(moreColors);
+var squares = [];
 
 
-// For loop to randomly add one color for each item from squares array (each square on the board)
-// for (let i = 0; i < squares.length; i++) {
-//   randomColors[i] = colors[Math.floor(Math.random() * colors.length)];
-// }
-// // Limit 2 of each color being put into randomColors
-
-
-$(document).ready(function () {
-  // Initialize Materialize components
-  M.AutoInit(); 
-
-  flipSquare();
-  
-  //default audio/background music?
-
-  // Button Clicks
-  // Solitaire Button Click
-  $('#solitaire').click(function () {
-    // Set game to play alone
-    // Display Solitaire rules
-    $('p').text("Click to turn over any two squares. If the two squares match, you win! If they don't match, click another square. Repeat as many times as you want!");
-    // $('.modal').modal('open');
-  });
-  // PvP Button Click
-  $('#twoPlayers').click(function () {
-    // Set game to play against another player
-    // Display PvP rules
-    $('p').text("Player 1 starts: Click to turn over any two squares. If the two squares match, you win! If they don't match, Player 2 takes a turn to click two squares. Repeat as many times as you want!");
-  });
-  // PvAi Button Click
-  $('#computer').click(function () {
-    // Set game to play against computer/ai
-    // Display PvAi rules
-    $('p').text("Click to turn over any two squares. If the two squares match, you win! If they don't match, the computer takes a turn to click two squares. Repeat as many times as you want!");
-  });
-  // Reset Button
-  $('#autorenew').click(function () {
-    $('.col').css('background-color', '');
-  });
-
-});
+function assignColors() {
+  for (let i = 0; i < squares.length; i++) {
+    $(squares[i]).css('background-color', randomColors[i]);
+  }
+}
 
 function flipSquare() {
   
   // click on square and change the color
   $('.col').click(function () {
     //Set random colors for each square
-    for (let i = 0; i < squares.length; i++) {
-      $(squares[i]).css('background-color', randomColors[i]);
-    }
-      // $(this).css("background-color", );
+    $(this).css("background-color", );
   });
 }
 // if/else statement so that only 1 square has color visible at a time
@@ -101,4 +56,42 @@ function flipSquare() {
 //  Player vs. Player Mode
 //  Player vs. AI Mode
 
+$(document).ready(function () {
+  // Initialize Materialize components
+  M.AutoInit();
 
+  flipSquare();
+
+  //default audio/background music?
+
+  // Button Clicks
+
+  // Solitaire Button Click
+  $('#solitaire').click(function () {
+    // Set game to play alone
+    // Display Solitaire rules
+    $('p').text("Click to turn over any two squares. If the two squares match, you win! If they don't match, click another square. Repeat as many times as you want!");
+    // $('.modal').modal('open');
+  });
+
+  // PvP Button Click
+  $('#twoPlayers').click(function () {
+    // Set game to play against another player
+    // Display PvP rules
+    $('p').text("Player 1 starts: Click to turn over any two squares. If the two squares match, you win! If they don't match, Player 2 takes a turn to click two squares. Repeat as many times as you want!");
+  });
+
+  // PvAi Button Click
+  $('#computer').click(function () {
+    // Set game to play against computer/ai
+    // Display PvAi rules
+    $('p').text("Click to turn over any two squares. If the two squares match, you win! If they don't match, the computer takes a turn to click two squares. Repeat as many times as you want!");
+  });
+
+  // Reset Button
+  $('#autorenew').click(function () {
+    $('.col').css('background-color', '');
+    // randomColors needs to refresh when this button is clicked!
+  });
+
+});
