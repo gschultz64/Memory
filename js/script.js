41e2546dff6a8cbe3308ffffa31bee47ef18ea58
Genevieve <genschultz@gmail.com>
2018-06-09 16:40:28 -0700

    updated turn layout. improved move logic.

--- js/script.js
+++ js/script.js
@@ -1,9 +1,7 @@
 // global variables
 
-// declaring move variable
 var moves = 0;
-var counter = $(".moves");
-// declaring colors available for the game board
+// colors available for the game board
 var colors = [
   '#3C989E', '#5DB5A4', '#F4CDA5', '#F57A82', '#E37B40', '#5E005E', '#AB2F52', '#41733F'
 ];
@@ -33,26 +31,22 @@ var squares = ['#0', '#1', '#2', '#3', '#4', '#5', '#6', '#7',
 
 // When player clicks on a square, change the background color to one of randomColors 
 function clickAction() {
-  if (moves >= 2) {
+  if (moves % 2 === 0) {
     for (let i = 0; i < squares.length; i++) {
       $(squares[i]).css("background-color", '');
-      moves = 0;
     }
   }
-
   var parsedId = parseInt(this.id);
   $(this).css("background-color", randomColors[parsedId]);
   console.log("box " + squares[parsedId] + " is " + randomColors[parsedId]);
   countMoves();
-  console.log("Click # " + moves);
+  $('.moves').text(moves);
   checkForMatch();
 }
 
 // count player moves
 function countMoves() {
   moves++;
-  // counter.text("moves");
-  // startTimer();
 }
 
 function flipSquare() {
@@ -62,11 +56,6 @@ function flipSquare() {
 
   }
 }
-
-// function winMessage() {
-//   // Alert player if they win or lose
-// }
-
 
 function checkForMatch() {
   // // Check for matching colors when two squares are "flipped"
@@ -80,7 +69,12 @@ function checkForMatch() {
     }
   } 
 }
-  // winMessage();
+
+// function winMessage() {
+// // Alert player if they win or lose
+// }
+
+// winMessage();
 
 // add disabled class to colors after match
 
@@ -98,17 +92,7 @@ function checkForMatch() {
 //   $('.timer').append("<p>" + "0.00:" + remainingTime + "</p>");
 //   console.log('timer revealed');
 // }
-// count player moves
-function countMoves() {
-  moves++;
-  // counter.text("moves");
-  // startTimer();
-}
-// win conditions
-//  two colored divs need to match exactly 
-// when colors are matched, remove from randomColors? or keep them active and unclickable
-//  Solitaire Mode
-//  Player vs. Player Mode
+
 
 $(document).ready(function () {
   // Initialize Materialize components
@@ -120,7 +104,6 @@ $(document).ready(function () {
   //default audio/background music?
 
   // Button Clicks
-
   // Solitaire Button Click
   $('#solitaire').click(function () {
     // Set game to play alone
@@ -128,20 +111,25 @@ $(document).ready(function () {
     $('p').text("Click to turn over any two squares. If the two squares match, you win! If they don't match, click another square. Repeat as many times as you want!");
     // $('.modal').modal('open');
   });
-
   // PvP Button Click
   $('#twoPlayers').click(function () {
     // Set game to play against another player
     // Display PvP rules
     $('p').text("Player 1 starts: Click to turn over any two squares. If the two squares match, you win! If they don't match, Player 2 takes a turn to click two squares. Repeat as many times as you want!");
   });
-
   // Reset Button
   $('#autorenew').click(function () {
     $('.col').css('background-color', '');
-    // randomColors needs to refresh when this button is clicked!
     shuffle(randomColors);
     moves = 0;
+    $('.moves').text(moves);
+    $('.turn').text('Player ')
   });
 
 });
+
+// win conditions
+//  two colored divs need to match exactly 
+// when colors are matched, remove from randomColors? or keep them active and unclickable
+//  Solitaire Mode
+//  Player vs. Player Mode
