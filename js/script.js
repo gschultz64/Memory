fed72844c53fdb8b2a6182a356e4c6974ca7de45
Genevieve <genschultz@gmail.com>
2018-06-11 01:00:34 -0700

    game logic: play until the board is full and a winner is determined.

--- js/script.js
+++ js/script.js
@@ -1,10 +1,19 @@
 
 // global variables
+// Variable to set initial moves count to 0
 var moves = 0;
+// Empty array to store the color on each move
 var moveArray = [];
+// Variable to set current player and scores
+var currentPlayer = 1;
+var playerOneScore = 0;
+var playerTwoScore = 0;
 // array to hold ids of each div with class "col"
 var squares = ['#0', '#1', '#2', '#3', '#4', '#5', '#6', '#7',
   '#8', '#9', '#10', '#11', '#12', '#13', '#14', '#15'];
+
+var squareOwners = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+
 // colors available for the game board
 var colors = [
   '#3C989E', '#5DB5A4', '#F4CDA5', '#F57A82', '#E37B40', '#5E005E', '#AB2F52', '#41733F'
@@ -30,17 +39,18 @@ function shuffle(array) {
 var randomColors = shuffle(moreColors);
 
 // count player moves (clicks on squares), add moves to the moveArray
-function countMoves(color) {
+function countMoves(id) {
   moves++;
-  moveArray.push(color);
-  // console.log(moveArray)
+  moveArray.push(id);
 }
 
 function takeTurn() {
   if (moves % 2 === 0) {
-    if ($('.turn').text() == 'Player 1') {
+    if (currentPlayer === 1) {
+      currentPlayer = 2;
       $('.turn').text('Player 2').removeClass('blue').addClass('yellow');
-    } else if ($('.turn').text() == 'Player 2') {
+    } else {
+      currentPlayer = 1;
       $('.turn').text('Player 1').removeClass('yellow').addClass('blue');
     }
   }
@@ -49,15 +59,36 @@ function takeTurn() {
 function checkForMatch() {
   // Check for matching colors when two squares are "flipped"
   if (moves % 2 === 0) {
-    // console.log(this);
-    if (moveArray[moveArray.length - 1] == moveArray[moveArray.length - 2]) {
-      $('.message').text('You won!').removeClass('red').addClass('green');
-      // console.log('You won')
+    var squareOneId = moveArray[moveArray.length - 1];
+    var squareTwoId = moveArray[moveArray.length - 2];
+    var lastMove = $("#"+ squareOneId).css('background-color');
+    var secondLastMove = $("#"+ squareTwoId).css('background-color');
+
+    if (lastMove == secondLastMove) {
+      squareOwners[squareOneId] = currentPlayer;
+      $("#"+ squareOneId).text(currentPlayer);
+      squareOwners[squareTwoId] = currentPlayer;
+      $("#"+ squareTwoId).text(currentPlayer);
+      $('.message').text("Player " + currentPlayer + ' made a match!').removeClass('red').addClass('green');
+      if (currentPlayer === 1) {
+        playerOneScore ++;
+      } else {
+        playerTwoScore ++;
+      }
+      if (playerOneScore + playerTwoScore === 8) {
+        if (playerOneScore > playerTwoScore) {
+          $('.message').text("Player One Wins!").removeClass('red').addClass('green');
+        } else {
+          $('.message').text("Player Two Wins!").removeClass('red').addClass('green');
+        }
+      }
     } else {
       $('.message').text("Try again.").removeClass('green').addClass('red');
-      // console.log('Try again.')
     }
-  } takeTurn();
+  }
+
+
+  takeTurn();
 }
 
 // When player clicks on a square, change the background color to one of randomColors array
@@ -69,14 +100,15 @@ function clickAction() {
   // If there have been an even number of moves (clicks on squares), remove all background colors from squares.
   if (moves % 2 === 0) {
     for (let i = 0; i < squares.length; i++) {
-      $(squares[i]).css("background-color", '');
+      if (squareOwners[i] === 0) {
+        $(squares[i]).css("background-color", '');
+      }
     }
   }
   // Get the id of square that was clicked on and reveal it's corresponding random color from the array randomColors.
   var parsedId = parseInt(this.id);
   $(this).css("background-color", randomColors[parsedId]);
-  // console.log("box " + squares[parsedId] + " is " + randomColors[parsedId]);
-  countMoves(randomColors[parsedId]);
+  countMoves(parsedId);
   // Display number of current moves 
   $('.moves').text(moves);
   // Check for matching colors 
@@ -90,7 +122,6 @@ function flipSquare() {
     $(squares[i]).on('click', clickAction);
   }
 }
-
 
 // add disabled class to colors after match
 
@@ -115,10 +146,10 @@ $(document).ready(function () {
     $('h3').text('Player vs. Player Game:');
     $('p').text("Player 1 starts: Click to turn over any two squares. If the two squares match, you win! If they don't match, Player 2 takes a turn to click two squares. Repeat as many times as you want!");
     // Reveal the turn message display, if it was previously hidden
-    $('.turn').css('visibility', 'visible');
+    $('.turn').css('visibility', 'visible').text('Player 1');
   });
   // Reset Button
-  $('#autorenew').click(function () {
+  $('#reset').click(function () {
     // Reset game so that no colors are visible
     $('.col').css('background-color', '');
     // Shuffle the randomColors array so that colors will appear in new positions
@@ -130,3 +161,7 @@ $(document).ready(function () {
     $('.message').text('').removeClass('green').addClass('red');
   });
 });
+
+//endless play mode
+//play until all matches are made
+//play until first match is made
